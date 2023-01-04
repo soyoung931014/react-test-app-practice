5836fa8703bb8b166edf19ff623c08e4203cda4f
park_so_young <soyoung931014@gmail.com>
2023-01-05 00:31:23 +0900

    feat: 모든 테스트 케이스 wrapper감싸기'

--- client/src/pages/OrderPage/tests/calculate.test.js
+++ client/src/pages/OrderPage/tests/calculate.test.js
@@ -1,11 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../../../test-utils";
 import userEvent from "@testing-library/user-event";
+import { OrderContextProvider } from "../../../contexts/OrderContext";
 import Type from "../Type";
 
 test("update products total when products change", async () => {
   render(<Type orderType="products" />);
-
-  const productsTotal = screen.getByText("상품 총 가격: ", { exact: false });
+  const productsTotal = screen.getByText("총 가격: ", { exact: false });
   expect(productsTotal).toHaveTextContent("0");
 
   // 아메리카 여행 상품 한개 올리기
